16acbd1b2633c1197d1d78566ecff24e4e1e026b
unknown <vorkapicmatija@gmail.com>
2020-09-18 12:49:29 +0200

    Add dependencies, permissions and a camera preview screen

--- app/src/main/java/com/example/qrcodereadercomposedemo/MainActivity.kt
+++ app/src/main/java/com/example/qrcodereadercomposedemo/MainActivity.kt
@@ -2,20 +2,16 @@ package com.example.qrcodereadercomposedemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
 import com.example.qrcodereadercomposedemo.ui.QRCodeReaderComposeDemoTheme
+import com.example.qrcodereadercomposedemo.ui.camera.CameraPreview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             QRCodeReaderComposeDemoTheme {
-
+                CameraPreview()
             }
         }
     }
--- app/src/main/java/com/example/qrcodereadercomposedemo/ui/camera/CameraPreview.kt
+++ app/src/main/java/com/example/qrcodereadercomposedemo/ui/camera/CameraPreview.kt
@@ -1,2 +1,45 @@
 package com.example.qrcodereadercomposedemo.ui.camera
 
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.platform.LifecycleOwnerAmbient
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+
+@Composable
+fun CameraPreview() {
+    val lifecycleOwner = LifecycleOwnerAmbient.current
+    val context = ContextAmbient.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    AndroidView(viewBlock = ::PreviewView) { previewView ->
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(
+                lifecycleOwner,
+                previewView,
+                cameraProvider)
+        }, ContextCompat.getMainExecutor(context))
+    }
+}
+
+fun bindPreview(
+    lifecycleOwner: LifecycleOwner,
+    previewView: PreviewView,
+    cameraProvider: ProcessCameraProvider
+) {
+    val preview: Preview = Preview.Builder().build()
+
+    val cameraSelector: CameraSelector = CameraSelector.Builder()
+        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+        .build()
+
+    preview.setSurfaceProvider(previewView.surfaceProvider)
+
+    cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview)
+}
